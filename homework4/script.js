c939e201705c1abea8bdaad5a05e2aa52bb97cc5
Vladyslav <133018052+Vinum96@users.noreply.github.com>
2023-06-07 18:51:04 +0200

    Add files via upload

---
+++ homework4/script.js
@@ -0,0 +1,38 @@
+// // Задача 1.
+// const number = +prompt("Введите число")
+// console.log(number * 0.1)
+
+// Задача 2.
+
+// const number1 = +prompt('Введите первое число');
+// const number2 = +prompt('Введите второе число');
+// if (number1 < number2) {
+// console.log(number1)
+// }
+// else{
+//     console.log(number2)}
+
+
+//  Задача 3.
+
+// const number = +prompt("Введите число")
+// if (number > 100) {
+//     console.log("Больше 100")
+// } else if(number == 100){
+//     console.log("Равно 100")
+
+// } 
+// else {
+//     console.log("Меньше 100")
+// }
+
+
+// Задача 4
+// const name = prompt("Введите ваше имя")
+// const age = +prompt("Введите ваш возраст")
+// if(age > 18) {
+//     console.log(`Hello, ${name}`)
+// }
+// else if(age < 18) {
+//     console.log(`Hi, ${name}`)
+// }
