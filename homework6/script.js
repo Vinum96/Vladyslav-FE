a7dcd19f047aad150b90f10a66476c19572f1072
Vladyslav <133018052+Vinum96@users.noreply.github.com>
2023-06-18 19:16:39 +0200

    Add files via upload

---
+++ homework6/script.js
@@ -0,0 +1,37 @@
+                    // ЗАдача 1
+// const numbersContainer = document.querySelector(".numbers")
+// for(let i = 100; i >= 50; i-=10 );
+// const paragraph = document.createElement("p");
+// numbersContainer.appendChild(paragraph);
+
+
+
+                        // Задача 2
+//     const strings = ['Hello', 'World', 'What', 'Array', 'Smooth'];
+//     const stringsContainer = document.querySelector('.strings_container');
+//     for (let i = 0; i < strings.length; i++) {
+//     const paragraph = document.createElement('p');
+//     paragraph.textContent = strings[i];
+//     stringsContainer.appendChild(paragraph);
+// }
+
+
+
+                            // Задача 3
+// const users = [
+//     { first_name: 'Vlad', last_name: 'Oliinyk', age: 26 },
+//     { first_name: 'Arsen', last_name: 'Vlasovic', age: 18 },
+//     { first_name: 'Nastia', last_name: 'Arbenina', age: 33 },
+//     { first_name: 'Lisa', last_name: 'Vlasenko', age: 12 }
+//   ];
+//     const usersContainer = document.querySelector('.users_container');
+//     for (let i = 0; i < users.length; i++) {
+//     if (users[i].age >= 18) {
+//     const card = document.createElement('div');
+//     card.classList.add('card');
+//     const cardContent = `<h3>${users[i].first_name} ${users[i].last_name}</h3> <p>Age: ${users[i].age}</p>`;
+//     card.innerHTML = cardContent;
+//     usersContainer.appendChild(card);
+//     }
+//   }
+                             
